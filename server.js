5881f12903dc85b979ba30f5e7834c76c6fab2ed
janulius <janu.lius021@gmail.com>
2018-07-26 11:12:35 +0700

    - + Prerender In-Memory Cache plugin - set CACHE_MAXSIZE=1000 and CACHE_TTL=600

--- server.js
+++ server.js
@@ -7,5 +7,6 @@ server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
+server.use(require('prerender-memory-cache'))
 
 server.start();
